c46de01430904259eb05e2b2eba6e015347812b4
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-04-26 11:07:34 -0700

    Use more appropriate sed delimiter for secret keys

--- makeLaunchScript.sh
+++ makeLaunchScript.sh
@@ -7,10 +7,10 @@
 . ./autoscaling_vars.sh
 
 cat launch.sh.template | \
-   sed -e "s/YOUR_ACCESS_KEY_ID/$aws_access_key_id/g; \
-     s/YOUR_SECRET_ACCESS_KEY/$aws_secret_access_key/g; \
-     s/YOUR_REGION/$region/g; \
-     s/YOUR_AUTOSCALING_GROUP_NAME/$autoscaling_group_name/g"\
+   sed -e "s#YOUR_ACCESS_KEY_ID#${aws_access_key_id}#g; \
+     s#YOUR_SECRET_ACCESS_KEY#${aws_secret_access_key}#g; \
+     s#YOUR_REGION#$region#g; \
+     s#YOUR_AUTOSCALING_GROUP_NAME#$autoscaling_group_name#g"\
      > launch.sh
 
 echo Created launch.sh
